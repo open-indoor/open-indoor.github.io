6666b4e9ba39fc8aaed07cd1a3f7a33ba9f31b4f
Dorian Fournier <dorian@openindoor.io>
2021-07-30 15:20:30 +0200

    Animation + details

--- examples/VisiteGallery/script.js
+++ examples/VisiteGallery/script.js
@@ -1,6 +1,6 @@
 var psList = [];
 var poiList = [];
-var scene, psMain, camera, imgLink, POI_button;
+var scene, psMain, camera, imgLink, POI_button_1, POI_button_2, POI_button_3;
 
 AFRAME.registerComponent('displacement', {
 
@@ -13,18 +13,24 @@ AFRAME.registerComponent('displacement', {
       var data = this.data;
       var el = this.el;
 
+      var position = new THREE.Vector3();
+      this.el.object3D.getWorldPosition(position);
+
       el.addEventListener("click", function(event) {
         psMain.setAttribute("src", psList[data.index].src);
-        addCrossOnPhotosphere(psList[data.index]);
+        //moveCamera(position.x,position.z);
+        psMain.addEventListener('materialtextureloaded', function() {
+          addElementOfNavigation(psList[data.index]);
+        });
       });
   },
 
 });
 
-function addCrossOnPhotosphere(origin) {
+function addElementOfNavigation(origin) {
 
   //Remove ancient cross
-  var listCross = document.querySelectorAll(".rot");
+  var listCross = document.querySelectorAll(".entity");
   listCross.forEach(currentValue => {
     currentValue.parentNode.removeChild(currentValue);
   });
@@ -39,14 +45,38 @@ function addCrossOnPhotosphere(origin) {
 
   //Bouton pour POI
   imgLink = origin.POI;
-  if(imgLink == null)
+  if(imgLink != null)
   {
-    POI_button.setAttribute("visible", "false")
+    switch(imgLink.length) {
+      case 1:
+        POI_button_1.setAttribute("visible", "true");
+        POI_button_1.setAttribute("material", "src: " + imgLink[0]);
+        POI_button_2.setAttribute("visible", "false");
+        POI_button_3.setAttribute("visible", "false");
+        break;
+      case 2:
+        POI_button_1.setAttribute("visible", "true");
+        POI_button_1.setAttribute("material", "src: " + imgLink[0]);
+        POI_button_2.setAttribute("visible", "true");
+        POI_button_2.setAttribute("material", "src: " + imgLink[1]);
+        POI_button_3.setAttribute("visible", "false");
+        break;
+      case 3:
+        POI_button_1.setAttribute("visible", "true");
+        POI_button_1.setAttribute("material", "src: " + imgLink[0]);
+        POI_button_2.setAttribute("visible", "true");
+        POI_button_2.setAttribute("material", "src: " + imgLink[1]);
+        POI_button_3.setAttribute("visible", "true");
+        POI_button_3.setAttribute("material", "src: " + imgLink[2]);
+        break;
+    } 
   } else {
-    POI_button.setAttribute("visible", "true")
+      POI_button_1.setAttribute("visible", "false");
+      POI_button_2.setAttribute("visible", "false");
+      POI_button_3.setAttribute("visible", "false");
   }
 
-  //Add new cross
+  //Add new cross and arrow
   origin.neighbourList.forEach(currentValue => {
     var index = psList.indexOf(currentValue);
 
@@ -58,23 +88,25 @@ function addCrossOnPhotosphere(origin) {
 
     var rot_box = document.createElement("a-entity");
     rot_box.setAttribute("id", "rot_box__" + index)
-    rot_box.setAttribute("class", "rot")
+    rot_box.setAttribute("class", "entity")
     rot_box.setAttribute("rotation", "0 " + (180 - bearing) + " 0");
     scene.appendChild(rot_box);
 
     var cross = document.createElement("a-obj-model");
     cross.setAttribute("id", "cross__" + index)
-    rot_box.setAttribute("class", "cross")
-    cross.setAttribute("position", distance + " -1.8 0");
-    cross.setAttribute("rotation", "0 0 0");
+    cross.setAttribute("class", "cross")
+    cross.setAttribute("position",  distance + " -1.8 0");
+    cross.setAttribute("rotation", "0 45 0");
     cross.setAttribute("scale", "0.5 0.5 0.5");
+    cross.setAttribute("event-set__mouseenter", "scale: 0.8 0.8 0.8");
+    cross.setAttribute("event-set__mouseleave", "scale: 0.5 0.5 0.5");
     cross.setAttribute("displacement", "index: "+ index);
     cross.setAttribute("src", "#marker");
     rot_box.appendChild(cross);
 
     var arrow = document.createElement("a-obj-model");
     arrow.setAttribute("id", "arrow__" + index)
-    rot_box.setAttribute("class", "arrow")
+    arrow.setAttribute("class", "arrow")
     arrow.setAttribute("position", 1.5 + " -1.8 0");
     arrow.setAttribute("rotation", "0 90 0");
     arrow.setAttribute("scale", "0.3 0.3 0.3");
@@ -84,23 +116,29 @@ function addCrossOnPhotosphere(origin) {
   });
 };
 
-function run() {
+function run(onTrue) {
   camera = document.getElementById("camera");
   psMain = document.getElementById("main-ps");
   psMain.setAttribute("src", psList[0].src);
-  POI_button = document.querySelector('#refresh-button');
-  document.querySelector('#refresh-button').addEventListener('click', function() {
-    imgLink.forEach(element => {
-      window.open(element, '_blank', 'location=yes,height=windows.height,width=windows.width,scrollbars=yes,status=yes');
-    });
+  POI_button_1 = document.querySelector('#refresh-button-1');
+  POI_button_2 = document.querySelector('#refresh-button-2');
+  POI_button_3 = document.querySelector('#refresh-button-3');
+  document.querySelector('#refresh-button-1').addEventListener('click', function onClick() {
+    window.open(imgLink[0], '_blank', 'location=yes,height=windows.height,width=windows.width,scrollbars=yes,status=yes');
   });
-  addCrossOnPhotosphere(psList[0])
+  document.querySelector('#refresh-button-2').addEventListener('click', function onClick() {
+    window.open(imgLink[1], '_blank', 'location=yes,height=windows.height,width=windows.width,scrollbars=yes,status=yes');
+  });
+  document.querySelector('#refresh-button-3').addEventListener('click', function onClick() {
+    window.open(imgLink[2], '_blank', 'location=yes,height=windows.height,width=windows.width,scrollbars=yes,status=yes');
+  });
+  addElementOfNavigation(psList[0])
 };
 
 var getValues = function(){
   scene = document.querySelector("a-scene");
   if (scene.hasLoaded) {
-    run();
+    run(true);
   } else {
     scene.addEventListener("loaded", run);
   }
@@ -163,3 +201,27 @@ $.getJSON("https://vegeta.openindoor.io/indoor/data/ArtGallery_V3.geojson", func
 
   getValues();
 });
+
+/*
+var start, x_start, y_start, x_rotation, y_rotation;
+var duration = 1000;
+
+function moveCamera(x_destination, y_destination) {
+    start = null;
+    x_start = camera.components["look-controls"].pitchObject.rotation.x;
+    x_rotation = x_destination - x_start;
+    y_start = camera.components["look-controls"].yawObject.rotation.y;
+    y_rotation = y_destination - y_start;
+    requestAnimationFrame(step);
+}
+
+function step(timestamp) {
+    if (start === null) start = timestamp;
+    var progress = timestamp - start;
+    camera.components["look-controls"].pitchObject.rotation.x = x_start + x_rotation * progress / duration;
+    camera.components["look-controls"].yawObject.rotation.y = y_start + y_rotation * progress / duration;
+    if (progress < duration) {
+        requestAnimationFrame(step);
+    }
+}
+*/
