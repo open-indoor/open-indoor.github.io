9e6f487ddbae91f9ac2043ced74447047471208d
clement.igonet@openindoor.io <clement.igonet@openindoor.io>
2021-08-07 13:50:16 +0000

    add token to tegola.opennindoor.io requests

--- examples/oid5-frontend/app.js
+++ examples/oid5-frontend/app.js
@@ -31,7 +31,7 @@ let mapStyle = {
         },
         "openindoor": {
             'type': 'vector',
-            "tiles": ["https://tegola.openindoor.io/maps/openindoor/{z}/{x}/{y}.vector.pbf?token=xxx"],
+            "tiles": ["https://tegola.openindoor.io/maps/openindoor/{z}/{x}/{y}.vector.pbf?token=kjGxKVMZcXAao1mtOOALxWY3"],
             cluster: true,
             "tolerance": 0
         }
